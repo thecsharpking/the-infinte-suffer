5169eb056dcb38f6b0f949d49d968f49f79d9c3c
Metehan <Metehan@DESKTOP-3R38B1C>
2022-08-18 11:55:46 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("what is your school number: ");
+string schoolnumber = Console.ReadLine();
+
+
+while (1 == 1)
+{
+    Console.Write("YOU ARE GONA SUFFER! HAHAHAHAHAHAHAHAHAHAH");
+}
